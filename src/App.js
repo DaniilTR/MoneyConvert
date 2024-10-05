c571e6e0e89e4050ebeb4ef3031d223efff01313
DaniilTR <qwertyasd.228336@gmail.com>
2024-10-05 23:09:54 +0500

    asdasd

--- src/App.js
+++ src/App.js
@@ -3,7 +3,41 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <header className='Head'>
+        <div className="Logo_Name">
+          <h2>MoneyConvert</h2>
+        </div>
+      </header>
+      <section>
+        <div className="main">
+          <div className='main_flex'>
+            <div className='convInput'>
+              <input 
+                className="Textinput-Control" 
+                inputmode="decimal" 
+                maxlength="20"
+                defaultValue="0" 
+              />
+              <span className='buttonSelect'>
+                <button></button>
+              </span>
+            </div>
+            <button className='Buttonconv'></button>
+            <div className='convInput'>
+              <input   
+                className="Textinput-Control" 
+                inputmode="decimal" 
+                maxlength="20" 
+                defaultValue="0" 
+              />
+              <span className='buttonSelect'>
+                <button></button>
+              </span>
+            </div>
+          </div>
+        </div>
+        <h3 style={{margin: '25px'}}> Актуальный курс валют </h3>
+      </section>
     </div>
   );
 }
